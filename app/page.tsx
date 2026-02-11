2cd73f69942ad8e4a48371d1ec1648d123e3f13f
Zavala TechLabs <zavala.techlabs@gmail.com>
2026-02-11 16:14:51 +0000

    fix: update rotating pies - 2D rotation, real images, replace grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { HeroSection } from '@/components/sections/HeroSection'
 import { FeaturedPiesRow } from '@/components/sections/FeaturedPiesRow'
 import { FeaturesSection } from '@/components/sections/FeaturesSection'
-import { MenuPreviewSection } from '@/components/sections/MenuPreviewSection'
 import { ReviewsMarquee } from '@/components/sections/ReviewsMarquee'
 
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
       <HeroSection />
       <FeaturedPiesRow />
       <FeaturesSection />
-      <MenuPreviewSection />
       <ReviewsMarquee />
     </>
   )
